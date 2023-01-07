ab8fdb1a0905a3a5480967c15cb950a62138eea4
anightatheopera <ritalino7@hotmail.com>
2023-01-07 14:22:22 +0000

    mudar para o outro pc

--- DataAccessLibrary/Model/BarracaModel.cs
+++ DataAccessLibrary/Model/BarracaModel.cs
@@ -7,7 +7,6 @@
         public int Id_Barraca { get; set; }
         public string Url_logotipo { get; set; }
         public string Id_Vendedor { get; set; }
-
         public string Id_Feira { get; set; }
         public virtual UtilizadorModel Vendedor { get; set; }
         public virtual FeiraModel Feira { get; set; }
