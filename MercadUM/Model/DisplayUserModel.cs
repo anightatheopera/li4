f10c04613633966da954fbd9a7ccad3e8d0bd092
anightatheopera <ritalino7@hotmail.com>
2023-01-13 19:57:09 +0000

    para guardar este estado

---
+++ MercadUM/Model/DisplayUserModel.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MercadUM.Model {
+    public class DisplayUtilizadorModel
+    {
+        [Required]
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+
+        [Required]
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Password { get; set; }
+
+        [Required]
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Nome { get; set; }
+
+        [Required]
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string DataNascimento { get; set; }
+
+        [Required]
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string TipoDeConta { get; set; }
+
+        [Required]
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Nr_Telemovel { get; set; }
+
+        [Required]
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Morada { get; set; }
+
+        [Required]
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string Pagamento { get; set; }
+
+    }
+}
--- MercadUM/Program.cs
+++ MercadUM/Program.cs
@@ -1,22 +1,7 @@
-using MercadUM.Areas.Identity;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using System.Data.SqlClient;
-using System.Data;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Syncfusion.Blazor;
-using Microsoft.OData.Edm;
-using DataAccessLibrary.Data;
-using System.Configuration;
-using MercadUM.Data;
+using DataAccessLibrary;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Logging.ClearProviders();
@@ -24,21 +9,13 @@ builder.Logging.AddConsole();
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("Default");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-               options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddIdentity<UtilizadorModel, IdentityRole>()
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<UtilizadoresData>();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
 builder.Services.AddTransient<ISqlDataAccess, SqlDataAccess>();
-builder.Services.AddTransient<UtilizadoresData>();
+builder.Services.AddTransient<IUtilizadoresData, UtilizadoresData>();
 builder.Services.AddTransient<IFeirasData, FeirasData>();
 builder.Services.AddTransient<IBarracasData, BarracasData>();
-builder.Services.AddTransient<IProdutosData, ProdutosData>();
-builder.Services.AddSyncfusionBlazor();
+builder.Services.AddTransient<IProdutosData, ProdutosData>();builder.Services.AddSyncfusionBlazor();
 
 
 var app = builder.Build();
@@ -72,8 +49,9 @@ app.Run();
 
 namespace MercadUM
 {
-    class Program { 
-    static void Main(string[] args)
+    class Program
+    {
+        static void Main(string[] args)
         {
             string cs = "Data Source=mercadum.database.windows.net;Initial Catalog=MercadUM;User ID=mercadum;Password=Grupo#8UM;Connect Timeout=60;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
@@ -87,14 +65,14 @@ namespace MercadUM
 
             con.Open();
 
-            SqlDataReader rdr =  cmd.ExecuteReader();
+            SqlDataReader rdr = cmd.ExecuteReader();
 
-            if(rdr.HasRows)
+            if (rdr.HasRows)
             {
-                while(rdr.Read())
+                while (rdr.Read())
                 {
                     Console.WriteLine("{0}    {1}    {2}     {3}    {4}"
-                        ,rdr["nome"],rdr["email"],rdr["data_nasc"],rdr["morada"],rdr["pagamento"]);
+                        , rdr["nome"], rdr["email"], rdr["data_nasc"], rdr["morada"], rdr["pagamento"]);
                 }
             }
             con.Close();
